691dab245eedbd6b823712bba1412d834b55c14c
tplanes <tho.planes@gmail.com>
2024-07-15 12:16:13 +0200

    now building nginx container to add auth via with start script

---
+++ nginx/start_nginx.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Create auth data to access the user interface (SHA-256)
+printf "$UI_USER:$(openssl passwd -5 $UI_PASSWD)\n" >> /etc/nginx/.htpasswd
+
+# Start nginx as foreground process
+nginx -g 'daemon off;'
