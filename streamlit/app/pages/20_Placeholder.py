ce8a389cd7adfcab96ac1ddc4380a275b7ca6411
tplanes <tho.planes@gmail.com>
2024-07-15 20:37:09 +0200

    draft selection of sensor and digitizer to define response

--- streamlit/app/pages/20_Placeholder.py
+++ streamlit/app/pages/20_Placeholder.py
@@ -13,6 +13,7 @@ st.title('Add stations')
 
 # Instrument and responses online catalog
 # todoldeprecated: need to use v2 offline copy instead...
+# todo: verify sensor and datalogger keys "depth"
 nrl = NRL()
 
 st.write("New station")
@@ -22,15 +23,31 @@ net = st.text_input("Network code", value="", max_chars=2, type="default", help=
 stat = st.text_input("Station code", value="", max_chars=5, type="default", help=None, placeholder="????")
 loc = st.text_input("Location code", value="00", max_chars=2, type="default", help=None, placeholder="00") 
 
-manufacturer = st.selectbox("Select sensor manufacturer", nrl.sensors.keys(), index=None, placeholder="Choose an option")
-sensor_choice = [] if manufacturer is None else nrl.sensors[manufacturer].keys()
-sensor = st.selectbox("Select sensor", sensor_choice, index=None, placeholder="Choose an option", disabled=manufacturer is None)
-model = None
-if sensor is not None and isinstance(nrl.sensors[manufacturer][sensor], dict):
-    #model_choice = [] if sensor is None else nrl.sensors[manufacturer][sensor].keys()
-    model_choice = nrl.sensors[manufacturer][sensor].keys()
-    model = st.selectbox("Select model", model_choice, index=None, placeholder="Choose an option")
-model
+sensor_manufacturer = st.selectbox("Select sensor manufacturer", nrl.sensors.keys(), index=None, placeholder="Choose an option")
+sensors = [] if sensor_manufacturer is None else nrl.sensors[sensor_manufacturer].keys()
+sensor = st.selectbox("Select sensor", sensors, index=None, placeholder="Choose an option", disabled=sensor_manufacturer is None)
+sensor_param = None
+if sensor is not None and isinstance(nrl.sensors[sensor_manufacturer][sensor], dict):
+    sensor_params = nrl.sensors[sensor_manufacturer][sensor].keys()
+    sensor_param = st.selectbox(nrl.sensors[sensor_manufacturer][sensor].__str__(), sensor_params, index=None, placeholder="Choose an option")
+sensor_param
+ADC_manufacturer = st.selectbox("Select digitizer manufacturer", nrl.dataloggers.keys(), index=None, placeholder="Choose an option")
+ADCs = [] if ADC_manufacturer is None else nrl.dataloggers[ADC_manufacturer].keys()
+ADC = st.selectbox("Select digitizer", ADCs, index=None, placeholder="Choose an option", disabled=ADC_manufacturer is None)
+ADC_gain = None
+if ADC is not None and isinstance(nrl.dataloggers[ADC_manufacturer][ADC], dict):
+    ADC_gains = nrl.dataloggers[ADC_manufacturer][ADC].keys()
+    ADC_gain = st.selectbox(nrl.dataloggers[ADC_manufacturer][ADC].__str__(), ADC_gains, index=None, placeholder="Choose an option")
+ADC_sample_rate = None
+if ADC_gain is not None and isinstance(nrl.dataloggers[ADC_manufacturer][ADC][ADC_gain], dict):
+    ADC_sample_rates = nrl.dataloggers[ADC_manufacturer][ADC][ADC_gain].keys()
+    ADC_sample_rate = st.selectbox(nrl.dataloggers[ADC_manufacturer][ADC][ADC_gain].__str__(), ADC_sample_rates, index=None, placeholder="Choose an option")
+
+response = nrl.get_response(
+    sensor_keys=[sensor_manufacturer, sensor, sensor_param],
+    datalogger_keys=[ADC_manufacturer, ADC, ADC_gain, ADC_sample_rate]
+)
+response
 
 with st.form("new_station_form"):
     checkbox_val = st.checkbox("Form checkbox")
