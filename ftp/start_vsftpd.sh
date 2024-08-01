13a55d0d7c57e7c87e1776feac4cc19e6afa7e3a
tplanes <tho.planes@gmail.com>
2024-08-01 12:53:07 +0000

    vsftpd container stopping debug in progress

--- ftp/start_vsftpd.sh
+++ ftp/start_vsftpd.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
-# Add initial station if defined in .env
-#if [[ -z "$STATION_NAME" ]] || [[ -z "$STATION_PASSWD" ]]; then
-#    echo "Initial station name or password empty, skipping station creation..."
-#else
-#    echo -n "Creating $STATION_NAME virtual vsftpd user..."
-#    mkdir "/data/ftp/$STATION_NAME" && chown vsftpd:nogroup "/data/ftp/$STATION_NAME"
-#    cat > /etc/vsftpd/vusers.txt <<EOF
-#$STATION_NAME
-#$STATION_PASSWD
-#EOF
-#    db_load -T -t hash -f /etc/vsftpd/vusers.txt /etc/vsftpd/vsftpd-virtual-user.db # todo need to save user database in persistent volume!
-#    chmod 600 /etc/vsftpd/vsftpd-virtual-user.db
-#    rm /etc/vsftpd/vusers.txt
-#    echo "done"
-#fi
-
 # Generate ssl certificate (should do only once before first docker run instead?)
 <<EOF openssl req -x509 -nodes -days 1825 -newkey rsa:2048 -keyout /etc/ssl/certs/netrisk.key -out /etc/ssl/certs/netrisk.pem
 $COUNTRY_CODE
@@ -26,6 +10,26 @@ $COMMON_NAME
 $EMAIL_ADRESS
 EOF
 
+# Start vsftpd as foreground process, exec for replacing bash PID and receive docker signals (does not work)
+#exec /usr/sbin/vsftpd vsftpd.conf
 
-# Start vsftpd as foreground process, exec for replacing bash PID and receive docker signals
-exec /usr/sbin/vsftpd vsftpd.conf
+# signal trap function inspired by https://github.com/panubo/docker-vsftpd/blob/main/entry.sh
+vsftpd_stop() {
+  echo "Received SIGINT or SIGTERM. Shutting down vsftpd"
+  # Get PID
+  pid="$(cat /var/run/vsftpd/vsftpd.pid)"
+  # Set TERM
+  kill -SIGTERM $pid
+  # Wait for exit
+  wait $pid
+  # All done.
+  echo "Done"
+}
+
+trap vsftpd_stop SIGINT SIGTERM
+echo "Starting vsftpd"
+#/usr/sbin/vsftpd vsftpd.conf &
+sleep infinity &
+pid=$!
+echo $pid > /var/run/vsftpd/vsftpd.pid
+wait $pid && exit $?
