fc83e480f56c739124dcf392b1cdc7a61264ca86
tplanes <tho.planes@gmail.com>
2024-08-02 09:05:43 +0000

    reverted to simpler vsftpd start script and reduced number of published ports from 500 to 50: long stop bug fixed

--- ftp/start_vsftpd.sh
+++ ftp/start_vsftpd.sh
@@ -10,26 +10,26 @@ $COMMON_NAME
 $EMAIL_ADRESS
 EOF
 
-# Start vsftpd as foreground process, exec for replacing bash PID and receive docker signals (does not work)
-#exec /usr/sbin/vsftpd vsftpd.conf
+# Start vsftpd as foreground process, exec for replacing bash PID and receive docker signals (works only if init: true in compose(?))
+exec /usr/sbin/vsftpd vsftpd.conf
 
+# alternative way to catch TERM signal without init: true (which is better?)
 # signal trap function inspired by https://github.com/panubo/docker-vsftpd/blob/main/entry.sh
-vsftpd_stop() {
-  echo "Received SIGINT or SIGTERM. Shutting down vsftpd"
-  # Get PID
-  pid="$(cat /var/run/vsftpd/vsftpd.pid)"
-  # Set TERM
-  kill -SIGTERM $pid
-  # Wait for exit
-  wait $pid
-  # All done.
-  echo "Done"
-}
+#vsftpd_stop() {
+#  echo "Received SIGINT or SIGTERM. Shutting down vsftpd"
+#  # Get PID
+#  pid="$(cat /var/run/vsftpd/vsftpd.pid)"
+#  # Set TERM
+#  kill -SIGTERM $pid
+#  # Wait for exit
+#  wait $pid
+#  # All done.
+#  echo "Done"
+#}
 
-trap vsftpd_stop SIGINT SIGTERM
-echo "Starting vsftpd"
-/usr/sbin/vsftpd vsftpd.conf &
-#sleep infinity &
-pid=$!
-echo $pid > /var/run/vsftpd/vsftpd.pid
-wait $pid && exit $?
+#trap vsftpd_stop SIGINT SIGTERM
+#echo "Starting vsftpd"
+#/usr/sbin/vsftpd vsftpd.conf &
+#pid=$!
+#echo $pid > /var/run/vsftpd/vsftpd.pid
+#wait $pid && exit $?
