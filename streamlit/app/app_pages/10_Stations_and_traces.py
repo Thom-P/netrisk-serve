b59eb1f77e2000f1514047072609a754237fdede
tplanes <tho.planes@gmail.com>
2024-10-29 14:17:45 +0000

    starting Pep8 formatting

--- streamlit/app/app_pages/10_Stations_and_traces.py
+++ streamlit/app/app_pages/10_Stations_and_traces.py
@@ -3,6 +3,7 @@ import datetime
 
 import streamlit as st
 import pandas as pd
+from streamlit import session_state as sstate
 from streamlit_folium import st_folium
 from obspy.clients.fdsn import Client
 
@@ -10,36 +11,58 @@ from utils.obspy_plot_mod import ModifiedWaveformPlotting
 from utils.data_fetch import fetch_stations, get_trace, fetch_latest_data_times
 from utils.station_map import create_map, get_map_col_width
 from utils.station_infos import display_channels, display_availabilty
-from utils.trace_view import select_channels_and_dates, select_filter_params, fetch_trace_units
-from utils.trace_view import download_trace
+from utils.trace_view import (
+    select_channels_and_dates,
+    select_filter_params,
+    fetch_trace_units,
+    download_trace
+)
 
 
-st.header('Stations and traces') # st.title too big
+st.header('Stations and traces')  # st.title too big
 
 client = Client("http://seiscomp:8080")  # todo connection test here
 
 
-if "df_stations" not in st.session_state:
+if "df_stations" not in sstate:
     stations_txt = fetch_stations()
     if stations_txt is None:
-        st.info("You first need to create a station XML file and an FTP account for each of your stations.", icon="ℹ️")
+        st.info(
+            "You first need to create a station XML file and an FTP account "
+            "for each of your stations.",
+            icon="ℹ️"
+        )
         st.stop()
-    resp_types = {'Network': str, 'Station': str, 'SiteName': str} # to prevent auto conversion to int when num only names
-    # remove first char '#' (header line included as comment)
-    st.session_state.df_stations = pd.read_csv(io.StringIO(stations_txt[1:]), sep='|', dtype=resp_types)
-    st.session_state.df_stations["EndTime"].fillna("Running", inplace=True)
-    # should fetch here last time data was received for each station
+    # Prevent auto conversion to int when names are only numbers
+    resp_types = {
+        'Network': str,
+        'Station': str,
+        'SiteName': str
+    }
+    # Remove first char '#' (header line included as comment)
+    sstate.df_stations = pd.read_csv(
+        io.StringIO(stations_txt[1:]), sep='|', dtype=resp_types
+    )
+    sstate.df_stations["EndTime"].fillna("Active", inplace=True)
+    # Fetch status (latest time data was received for each station)
     df_latest = fetch_latest_data_times()
-    #insert df_latest last data received in df_stations
-    st.session_state.df_stations = st.session_state.df_stations.merge(df_latest, how='left', on=['Network', 'Station'])
-    st.session_state.df_stations['Status'].fillna('⚫', inplace=True)
-    # put status first
-    cols = st.session_state.df_stations.columns.tolist()
+    # Insert status in df_stations
+    if df_latest is not None:
+        sstate.df_stations = sstate.df_stations.merge(
+            df_latest, how='left', on=['Network', 'Station']
+        )
+        sstate.df_stations['Status'].fillna('⚫', inplace=True)
+    else:
+        sstate.df_stations['Status'] = '❓'
+    # Set Status as first column
+    cols = sstate.df_stations.columns.tolist()
     cols = cols[-1:] + cols[:-1]
-    st.session_state.df_stations = st.session_state.df_stations[cols]
+    sstate.df_stations = sstate.df_stations[cols]
 
-#inv = client.get_stations(level="station")  # can cache ? combine with previous fetch ?
-#net_codes = {net.code: ind for ind, net in enumerate(inv.networks)}  # need to test if empty
+# use client instead?
+# inv = client.get_stations(level="station")  # combine with previous fetch ?
+# net_codes = {net.code: ind for ind, net in enumerate(inv.networks)}
+# # need to test if empty
 
 
 # Map
@@ -47,7 +70,7 @@ m = create_map()
 
 col1, col2 = st.columns([0.6, 0.4])
 with col2:
-    st.text("") # hack for pseudo alignment of map
+    st.text("")  # Hack for pseudo alignment of map
     map_data = st_folium(m, width=get_map_col_width(), returned_objects=[])
     # call to render Folium map in Streamlit, but don't get any data back
     # from the map (so that it won't rerun the app when the user interacts)
@@ -59,10 +82,14 @@ tab1, tab2, tab3 = col1.tabs(["Station info", "Trace", "Day plot"])
 
 net, sta = None, None
 
-#### Station info
+# ****** Station info
 with tab1:
+    status_help = (
+        "Last data received: 🟢 - under an hour ago; 🟡 - under a day ago; "
+        "🔴 - over a day ago; ⚫ - no data"
+    )
     event = st.dataframe(
-        st.session_state.df_stations,
+        sstate.df_stations,
         hide_index=True,
         key=None,
         on_select="rerun",
@@ -70,27 +97,29 @@ with tab1:
         column_config={
             "Status": st.column_config.TextColumn(
                 "Status ⓘ",
-                help="Last data received: 🟢 - under an hour ago; 🟡 - under a day ago; 🔴 - over a day ago; ⚫ - no data",
-                #default="st.",
-                #max_chars=50,
-                #validate=r"^st\.[a-z_]+$",
+                help=status_help,
             )
-        }    
+        }
     )
 
     # Station selection in dataframe
     row_index = event.selection['rows']
     if not row_index:
-        st.info("Select station by ticking box in the leftmost column.", icon="ℹ️")
+        st.info(
+            "Select station by ticking box in the leftmost column.",
+            icon="ℹ️"
+        )
     else:
-        net, sta = st.session_state.df_stations.iloc[row_index[0]][['Network', 'Station']]
+        net, sta = sstate.df_stations.iloc[row_index[0]][[
+            'Network', 'Station'
+        ]]
         with st.expander('Channels'):
             display_channels(net, sta)
         with st.expander('Data availability'):
             display_availabilty(net, sta)
-        
 
-#### Trace viewer
+
+# ****** Trace viewer
 with tab2:
     if sta is None:
         st.write('Select a station in the previous tab.')
@@ -113,8 +142,8 @@ with tab2:
         #fig_deconv = plt.figure()
         #resp_plot_buffer = io.BytesIO()
         
-        #if "traces" not in st.session_state:
-        #    st.session_state.traces = None
+        #if "traces" not in sstate:
+        #    sstate.traces = None
         if st.button('View Trace', disabled=False if chans else True):
             with st.spinner('Fetching traces...'):
                 traces = get_trace(
@@ -124,7 +153,7 @@ with tab2:
                     start_date, end_date
                     )
                 if traces is None:
-                    st.session_state.traces = None
+                    sstate.traces = None
                     st.stop()
             if fmin is not None and fmax is not None:
                 with st.spinner('Filtering...'):
@@ -141,15 +170,15 @@ with tab2:
                     except Exception as err:
                         st.error(err, icon="🚨")
                         st.stop()
-            st.session_state.traces = traces
-            if st.session_state.traces is not None:
+            sstate.traces = traces
+            if sstate.traces is not None:
                 with st.spinner('Loading plot...'):
                     # nb: size (width, height), width will be adjusted to fit column container
                     height = 200 + 300 * len(chans)
                     width = height
-                    waveform = ModifiedWaveformPlotting(stream=st.session_state.traces, handle=True, size=(width, height))
+                    waveform = ModifiedWaveformPlotting(stream=sstate.traces, handle=True, size=(width, height))
                     fig = waveform.plot_waveform(handle=True)
-                    units = fetch_trace_units(st.session_state.traces[0], resp_remove)
+                    units = fetch_trace_units(sstate.traces[0], resp_remove)
                     fig.add_annotation(text=f"Amplitude ({units})", textangle=-90, xref='paper', xanchor='right', xshift=-90, x=0, yref='paper', y=0.5, showarrow=False)
                     st.plotly_chart(fig, use_container_width=True, theme=None)
                     st.info(f"Traces including more than {waveform.max_npts} samples ({int(waveform.max_npts / 100 / 60)} mins at 100Hz) are plotted using the low resolution min/max method (add ref). To interact with the fully resolved data, reduce the time window.", icon="ℹ️")
@@ -184,13 +213,13 @@ with tab3:  # need indep vars?
         st.markdown(f'## {net}.{sta}')
         
         col31, col32 = st.columns(2)
-        loc_codes = sorted(st.session_state.channel_df['Location'].unique().tolist())
+        loc_codes = sorted(sstate.channel_df['Location'].unique().tolist())
         loc = col31.selectbox("Select location", loc_codes, key="loc_day_plot")
-        chan_codes = st.session_state.channel_df.query('Location == @loc')['Channel'].unique(
+        chan_codes = sstate.channel_df.query('Location == @loc')['Channel'].unique(
         ).tolist()
         chan = col32.selectbox("Select channel", chan_codes)
-        # st.session_state['chans'] = st.multiselect("Select channel(s)", chan_codes)
-        # chans = st.session_state['chans']
+        # sstate['chans'] = st.multiselect("Select channel(s)", chan_codes)
+        # chans = sstate['chans']
 
         day = st.date_input('Day', value="default_value_today")
         start_date = datetime.datetime(day.year, day.month, day.day)
@@ -204,13 +233,13 @@ with tab3:  # need indep vars?
         #   # add validity check vs fs
 
 
-        if "day_traces" not in st.session_state:
-            st.session_state.day_traces = None
+        if "day_traces" not in sstate:
+            sstate.day_traces = None
         if st.button('View day plot', disabled=True if chan is None else False):
             with st.spinner('Fetching traces...'):
                 traces = get_trace(client, net, sta, loc, chan, start_date, end_date)
                 if traces is None:
-                    st.session_state.day_traces = None
+                    sstate.day_traces = None
                     st.stop()
             
             traces.detrend("linear") # otherwise cannot see anything
@@ -222,11 +251,11 @@ with tab3:  # need indep vars?
             
             #traces.merge(method=1)
 
-            st.session_state.day_traces = traces
+            sstate.day_traces = traces
             # add info about these preprocesses 
-            if st.session_state.day_traces is not None:
+            if sstate.day_traces is not None:
                 with st.spinner('Loading plot...'): 
-                    fig = st.session_state.day_traces.plot(handle=True, type='dayplot')
+                    fig = sstate.day_traces.plot(handle=True, type='dayplot')
                     # fig.axes[-1].set_xlabel('Time')
                     # fig.axes[-1].set_ylabel('Counts')
 
@@ -235,27 +264,27 @@ with tab3:  # need indep vars?
 
 
 ## Side bar selections
-#st.session_state['net'] = None
-#st.session_state['sta'] = None
-#st.session_state['chans'] = set()
+#sstate['net'] = None
+#sstate['sta'] = None
+#sstate['chans'] = set()
 
 #with st.sidebar:
-#    #index_preselect = net_codes[st.session_state['net']] if st.session_state['net'] is not None else None
+#    #index_preselect = net_codes[sstate['net']] if sstate['net'] is not None else None
 #    # widget should handle session state
-#    st.session_state['net'] = st.selectbox('Network', net_codes.keys(), placeholder="Choose a Network")
-#    net = st.session_state['net']
+#    sstate['net'] = st.selectbox('Network', net_codes.keys(), placeholder="Choose a Network")
+#    net = sstate['net']
 #    inv_stations = []
 #    if net is not None:
 #        inv_stations = inv.networks[net_codes[net]].stations
 #    sta_codes = (sta.code for sta in inv_stations)
-#    st.session_state['sta'] = st.selectbox('Station', sta_codes, placeholder="Choose a Station")
-#    sta = st.session_state['sta']
+#    sstate['sta'] = st.selectbox('Station', sta_codes, placeholder="Choose a Station")
+#    sta = sstate['sta']
 #    inv_detailed = None
 #    if sta is not None:
 #        inv_detailed = client.get_stations(network=net, station=sta, level="channel")
 #    chan_codes = set(chan.code for chan in inv_detailed.networks[0].stations[0]) if inv_detailed is not None else set()
-#    st.session_state['chans'] = st.multiselect("Select channel(s)", chan_codes)
-#    chans = st.session_state['chans']
+#    sstate['chans'] = st.multiselect("Select channel(s)", chan_codes)
+#    chans = sstate['chans']
 
 ## Matplot lib plots
 # fig = plt.figure()
@@ -263,7 +292,7 @@ with tab3:  # need indep vars?
 # st.pyplot(fig)
 # #traces.plot(fig=fig)
 
-#fig = st.session_state.traces.plot(handle=True)
+#fig = sstate.traces.plot(handle=True)
 #fig.axes[-1].set_xlabel('Time')
 #fig.axes[-1].set_ylabel('Counts')
 #st.pyplot(fig)
